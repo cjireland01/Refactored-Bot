4f8df07be89b2d10f2990afe48786b1939b4fddf
cjireland01 <cjireland01@gmail.com>
2025-09-30 20:33:39 -0400

    Change of database from .xlsx to SQLite

--- tasks/scheduleFinalLeaderboard.js
+++ tasks/scheduleFinalLeaderboard.js
@@ -3,14 +3,16 @@ const { finalPosition } = require("../features/endingLeaderboard");
 
 module.exports = {
     start(client) {
+        client.once("clientReady", () => {
         // Run at 3:15 AM EST (08:15 UTC)
         cron.schedule("15 8 * * *", () => {
             finalPosition(client);
         });
 
         // Run at 6:15 PM EST (23:15 UTC)
-        cron.schedule("15 23 * * *", () => {
+        cron.schedule("0 15 23 * * *", () => {
             finalPosition(client);
         });
+    });
     }
 };
